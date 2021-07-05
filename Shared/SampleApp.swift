e739af3c3c3ce97c0f57ab737606145fa9c94bcb
shydow <shydow@me.com>
2021-07-05 14:25:33 +0800

    Initial Commit

---
+++ Shared/SampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SampleApp.swift
+//  Shared
+//
+//  Created by Shydow Lee on 7/5/21.
+//
+
+import SwiftUI
+
+@main
+struct SampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
